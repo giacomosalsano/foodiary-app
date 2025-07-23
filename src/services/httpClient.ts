9aadd96a3a7dc4d7cc35ce8a15e07d3869034c22
Giacomo Salsano <giacomosalsano@hotmail.com>
2025-07-23 20:11:32 -0300

    fix: fixed useCreateMeal

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://hcnruwnjwa.execute-api.us-east-1.amazonaws.com',
 });
